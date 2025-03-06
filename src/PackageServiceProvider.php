2675424955c10310d25762edbc1f75ae99bb987a
Awais Safdar <awaissafdar412@gmail.com>
2025-03-06 16:22:38 +0500

    Update PackageServiceProvider.php

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -6,26 +6,42 @@ use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
 {
+    /**
+     * Register services.
+     */
     public function register()
     {
         // Merge package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/larpack.php', 'larpack');
+        $this->mergeConfigFrom(__DIR__ . '/config/larpack.php', 'larpack');
     }
 
+    /**
+     * Bootstrap services.
+     */
     public function boot()
     {
         // Load Routes
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        
-        // Load Views (Make sure your views are inside 'resources/views')
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'larpack');
-        
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+
+        // Load Views
+        $this->loadViewsFrom(__DIR__ . '/views/full-Admin-Panel', 'larpack');
+
         // Load Migrations
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
 
         // Publish Config
         $this->publishes([
-            __DIR__.'/../config/larpack.php' => config_path('larpack.php'),
+            __DIR__ . '/config/larpack.php' => config_path('larpack.php'),
         ], 'larpack-config');
+
+        // Publish Views (Optional)
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/larpack'),
+        ], 'larpack-views');
+
+        // Publish Migrations (Optional)
+        $this->publishes([
+            __DIR__ . '/migrations/' => database_path('migrations'),
+        ], 'larpack-migrations');
     }
 }
